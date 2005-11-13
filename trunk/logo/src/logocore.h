298328adaad86665cd253cbaf63362cceaeaaae4
david_costanzo <david_costanzo@9d0050d7-00b0-40f9-871c-8e02cbd5794f>
2005-11-13 06:04:25 +0000

    Merge in changes to logo.h from UCBLogo 5.5. This was done to get the file licensed under the GPL (the existing license conflicted with the GPL).
    git-svn-id: https://svn.code.sf.net/p/fmslogo/source@282 9d0050d7-00b0-40f9-871c-8e02cbd5794f

--- trunk/logo/src/logocore.h
+++ trunk/logo/src/logocore.h
@@ -1,12 +1,23 @@
 /*
-*      logocore.h          logo header file                dvb
-*
-*       Copyright (C) 1989 The Regents of the University of California
-*       This Software may be copied and distributed for educational,
-*       research, and not for profit purposes provided that this
-*       copyright and statement are included in all such copies.
-*
-*/
+ *      logocore.h          logo header file                dvb
+ *
+ *	Copyright (C) 1993 by the Regents of the University of California
+ *
+ *      This program is free software; you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation; either version 2 of the License, or
+ *      (at your option) any later version.
+ *  
+ *      This program is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *  
+ *      You should have received a copy of the GNU General Public License
+ *      along with this program; if not, write to the Free Software
+ *      Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ */
 
 #define ecma // for European extended character set using parity bit
 #define ibm
