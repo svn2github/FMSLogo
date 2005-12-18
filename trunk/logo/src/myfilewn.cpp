71289df54766eaebb17ef41ca3defd86bb1431e3
david_costanzo <david_costanzo@9d0050d7-00b0-40f9-871c-8e02cbd5794f>
2005-12-18 03:39:13 +0000

    Fix bug #1372735.
    Replace a problematic call to Editor->GetWindowText() with Editor->GetSubText().  GetWindowText() has been the root of two different bugs that cause FMSLogo to fail to save the contents of the editor.
    
    The latest bug only showed up on Win95 and it looks like GetWindowText() returned a 16-bit signed value that was cast to an int.  For example, a file of about 60000 bytes returned about -5600 bytes read.
    
    I have not encounted any problems with GetSubText() yet.
    
    
    git-svn-id: https://svn.code.sf.net/p/fmslogo/source@402 9d0050d7-00b0-40f9-871c-8e02cbd5794f

--- trunk/logo/src/myfilewn.cpp
+++ trunk/logo/src/myfilewn.cpp
@@ -183,56 +183,34 @@ bool TMyFileWindow::Write(const char * fileName)
       {
       memset(editBuffer, 0, editBufferLength);
 
-      // We ask for editBufferLength when we only care about
-      // windowTextLength bytes as a work-around for bug #1352838.
-      // The problem is that on some installs of Windows,
-      // ::GetWindowText() may write zero bytes and if the
-      // buffer isn't big enough to hold the string and the
-      // NUL-terminator, even though the MSDN documentation says 
-      // that the outputwill just be truncated.
-      // In any event, it's easy enough to ask for a few more bytes.
-      int bytesInEditBuffer = Editor->GetWindowText(editBuffer, editBufferLength);
-      if (bytesInEditBuffer != windowTextLength) 
+      Editor->GetSubText(editBuffer, 0, editBufferLength);
+
+      if (windowTextLength != 0 && editBuffer[windowTextLength - 1] != '\n')
          {
-         // we could not get the text the window
-         char buffer[256];
-         wsprintf(
-            buffer, 
-            "Unable to write file \"%s\" to disk\nError Code=%u.", 
-            fileName, 
-            GetLastError());
-         MessageBox(buffer, GetModule()->GetName(), MB_ICONEXCLAMATION | MB_OK);
+         editBuffer[windowTextLength + 0] = '\r';
+         editBuffer[windowTextLength + 1] = '\n';
          }
       else
          {
-         // we read the text from the window 
-         if (windowTextLength != 0 && editBuffer[windowTextLength - 1] != '\n')
-            {
-            editBuffer[windowTextLength + 0] = '\r';
-            editBuffer[windowTextLength + 1] = '\n';
-            }
-         else
-            {
-            editBufferLength = windowTextLength;
-            }
-
-         size_t bytesWritten = fwrite(
-            editBuffer,
-            sizeof(char),
-            editBufferLength,
-            file);
-         if (bytesWritten == editBufferLength)
-            {
-            success = true;
-            }
-
-         if (success)
-            {
-            Editor->ClearModify();
-            }
+         editBufferLength = windowTextLength;
          }
 
-         delete [] editBuffer;
+      size_t bytesWritten = fwrite(
+         editBuffer,
+         sizeof(char),
+         editBufferLength,
+         file);
+      if (bytesWritten == editBufferLength)
+         {
+         success = true;
+         }
+
+      if (success)
+         {
+         Editor->ClearModify();
+         }
+
+      delete [] editBuffer;
       }
    fclose(file);
 
@@ -282,6 +260,7 @@ void TMyFileWindow::CMSaveToWorkspace()
             "Your Edit has FAILED to load",
             MB_OK);
 
+         // "force" a change so that we still in "dirty" state
          error_happen = false;
          Editor->SetSelection(0, 0);
          Editor->Insert(" ");
