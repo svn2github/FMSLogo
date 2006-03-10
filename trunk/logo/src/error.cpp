25e68ec66600c53dd6a6ef4bcb4ec8bceed3b2d5
david_costanzo <david_costanzo@9d0050d7-00b0-40f9-871c-8e02cbd5794f>
2006-03-10 05:49:07 +0000

    Cosmetic changes: * Fix whitespace. * Move stack variable declarations closer to where they are first used. * Change local functions/variables that were extern to be declared static.
    git-svn-id: https://svn.code.sf.net/p/fmslogo/source@923 9d0050d7-00b0-40f9-871c-8e02cbd5794f

--- trunk/logo/src/error.cpp
+++ trunk/logo/src/error.cpp
@@ -22,15 +22,19 @@
 
 #include "allwind.h"
 
-NODE *throw_node = NIL;
-NODE *err_mesg = NIL;
+NODE *    throw_node = NIL;
 ERR_TYPES erract_errtype;
+
+static NODE * err_mesg = NIL;
 
 void err_print()
    {
-   CTRLTYPE save_flag = stopping_flag;
+   if (err_mesg == NIL) 
+      {
+      return;
+      }
 
-   if (err_mesg == NIL) return;
+   CTRLTYPE save_flag = stopping_flag;
 
    stopping_flag = RUN;
    print_backslashes = true;
@@ -63,301 +67,299 @@ NODE *err_logo(ERR_TYPES error_type, NODE *error_desc)
    bool recoverable = false;
    bool warning = false;
    bool uplevel = false;
-   NODE *err_act;
-   NODE *val = Unbound;
 
    ref(error_desc);
    switch (error_type)
       {
-       case FATAL:
-           prepare_to_exit(FALSE);
-           printfx("Logo: Fatal Internal Error.\n");
-           exit(1);
+      case FATAL:
+         prepare_to_exit(FALSE);
+         printfx("Logo: Fatal Internal Error.\n");
+         exit(1);
 
-       case OUT_OF_MEM_UNREC:
-           prepare_to_exit(FALSE);
-           printfx("Logo: Out of Memory.\n");
-           exit(1);
+      case OUT_OF_MEM_UNREC:
+         prepare_to_exit(FALSE);
+         printfx("Logo: Out of Memory.\n");
+         exit(1);
 
-       case OUT_OF_MEM:
-           use_reserve_tank();
-           err_mesg = reref(
-              err_mesg, 
-              cons_list(make_static_strnode("out of space")));
-           break;
+      case OUT_OF_MEM:
+         use_reserve_tank();
+         err_mesg = reref(
+            err_mesg, 
+            cons_list(make_static_strnode("out of space")));
+         break;
 
-       case STACK_OVERFLOW:
-           err_mesg = reref(
-              err_mesg, 
-              cons_list(make_static_strnode("stack overflow")));
-           break;
+      case STACK_OVERFLOW:
+         err_mesg = reref(
+            err_mesg, 
+            cons_list(make_static_strnode("stack overflow")));
+         break;
 
-       case TURTLE_OUT_OF_BOUNDS:
-           err_mesg = reref(
-               err_mesg, 
-               cons_list(make_static_strnode("turtle out of bounds")));
-           break;
+      case TURTLE_OUT_OF_BOUNDS:
+         err_mesg = reref(
+            err_mesg, 
+            cons_list(make_static_strnode("turtle out of bounds")));
+         break;
 
-       case BAD_GRAPH_INIT:
-           err_mesg = reref(
-               err_mesg, 
-               cons_list(make_static_strnode("couldn't initialize graphics")));
-           break;
+      case BAD_GRAPH_INIT:
+         err_mesg = reref(
+            err_mesg, 
+            cons_list(make_static_strnode("couldn't initialize graphics")));
+         break;
 
-       case BAD_DATA_UNREC:
-           err_mesg = reref(
-              err_mesg, 
-              cons_list(
-                 fun,
-                 make_static_strnode("doesn\'t like"),
-                 error_desc, 
-                 make_static_strnode("as input")));
-           break;
-
-       case DIDNT_OUTPUT:
-           if (didnt_output_name != NIL)
-              {
-              last_call = reref(last_call, didnt_output_name);
-              }
-           if (error_desc == NIL)
-              {
-              error_desc = vref(car(didnt_get_output));
-              ufun = reref(ufun, cadr(didnt_get_output));
-              this_line = reref(this_line, cadr(cdr(didnt_get_output)));
-              }
-           err_mesg = reref(
-              err_mesg, 
-              cons_list(
-                 last_call,
-                 make_static_strnode("didn\'t output to"),
-                 error_desc));
-           recoverable = true;
-           break;
-
-       case NOT_ENOUGH:
-           err_mesg = reref(
-              err_mesg, 
-              cons_list(
-                 make_static_strnode("not enough inputs to"), 
-                 fun));
-           break;
-
-       case BAD_DATA:
-           err_mesg = reref(
-              err_mesg, 
-              cons_list(
-                 fun,
-                 make_static_strnode("doesn\'t like"), 
-                 error_desc,
-                 make_static_strnode("as input")));
-           recoverable = true;
-           break;
-
-       case APPLY_BAD_DATA:
-           err_mesg = reref(
-              err_mesg, 
-              cons_list(
-                 make_static_strnode("APPLY doesn\'t like"),
-                 error_desc,
-                 make_static_strnode("as input")));
-           recoverable = true;
-           break;
-
-       case TOO_MUCH:
-           err_mesg = reref(
-              err_mesg,
-              cons_list(make_static_strnode("too much inside ()\'s")));
-           break;
-
-       case DK_WHAT_UP:
-           uplevel = true;
-           // FALLTHROUGH
-       case DK_WHAT:
-           err_mesg = reref(
-              err_mesg,
-              cons_list(
-                 make_static_strnode("You don\'t say what to do with"), 
-                 error_desc));
-           break;
-
-       case PAREN_MISMATCH:
-           err_mesg = reref(
-              err_mesg,
-              cons_list(make_static_strnode("too many (\'s")));
-           break;
-
-       case NO_VALUE:
-           err_mesg = reref(
-              err_mesg,
-              cons_list(
-                 error_desc,
-                 make_static_strnode("has no value")));
-           recoverable = true;
-           break;
-
-       case UNEXPECTED_PAREN:
-           err_mesg = reref(
-              err_mesg,
-              cons_list(make_static_strnode("unexpected \')\'")));
-           break;
-
-       case UNEXPECTED_BRACKET:
-           err_mesg = reref(
-              err_mesg,
-              cons_list(make_static_strnode("unexpected \']\'")));
-           break;
-
-       case UNEXPECTED_BRACE:
-           err_mesg = reref(
-              err_mesg,
-              cons_list(make_static_strnode("unexpected \'}\'")));
-           break;
-
-       case DK_HOW:
-           recoverable = true;
-           /* FALLTHROUGH */
-
-       case DK_HOW_UNREC:
-           err_mesg = reref(
-              err_mesg,
-              cons_list(
-                 make_static_strnode("I don\'t know how to"), 
-                 error_desc));
-           break;
-
-       case NO_CATCH_TAG:
-           err_mesg = reref(
-              err_mesg,
-              cons_list(
-                 make_static_strnode("Can't find catch tag for"), 
-                 error_desc));
-           break;
-
-       case ALREADY_DEFINED:
-           err_mesg = reref(
-              err_mesg,
-              cons_list(
-                 error_desc,
-                 make_static_strnode("is already defined")));
-           break;
-
-       case STOP_ERROR:
-           yield_flag = 1;
-           err_mesg = reref(
-              err_mesg,
-              cons_list(make_static_strnode("Stopping...")));
-           break;
-
-       case ALREADY_DRIBBLING:
-           err_mesg = reref(
-              err_mesg, 
-              cons_list(make_static_strnode("Already dribbling")));
-           break;
-
-       case FILE_ERROR:
-           err_mesg = reref(
-              err_mesg,
-              cons_list(
-                 make_static_strnode("File system error:"),
-                 error_desc));
-           break;
-
-       case IF_WARNING:
-           err_mesg = reref(
-              err_mesg,
-              cons_list(make_static_strnode("Assuming you mean IFELSE, not IF")));
-           warning = true;
-           break;
-
-       case SHADOW_WARN:
-           err_mesg = reref(
-              err_mesg, 
-              cons_list(
-                 error_desc,
-                 make_static_strnode("shadowed by local in procedure call")));
-           warning = true;
-           break;
-
-       case USER_ERR:
-           if (error_desc == Unbound)
-              {
-              err_mesg = reref(
-                 err_mesg,
-                 cons_list(make_static_strnode("Throw \"Error")));
-              }
-           else
-              {
-              uplevel = true;
-              if (is_list(error_desc))
-                 {
-                 err_mesg = reref(err_mesg, error_desc);
-                 }
-              else
-                 {
-                 err_mesg = reref(err_mesg, cons_list(error_desc));
-                 }
-              }
-           break;
-
-       case IS_PRIM:
-           err_mesg = reref(
-              err_mesg,
-              cons_list(
-                 error_desc,
-                 make_static_strnode("is a primitive")));
-           break;
-
-       case NOT_INSIDE:
-           err_mesg = reref(
-              err_mesg,
-              cons_list(make_static_strnode("Can't use TO inside a procedure")));
-           break;
-
-       case AT_TOPLEVEL:
-           err_mesg = reref(
-              err_mesg,
-              cons_list(
-                 make_static_strnode("Can only use"),
-                 error_desc,
-                 make_static_strnode("inside a procedure")));
-           break;
-
-       case NO_TEST:
-           err_mesg = reref(
-              err_mesg, 
-              cons_list(
-                 fun,
-                 make_static_strnode("without TEST")));
-           break;
-
-       case ERR_MACRO:
-           err_mesg = reref(
-              err_mesg,
-              cons_list(
-                 make_static_strnode("Macro returned"), 
-                 error_desc,
-                 make_static_strnode("instead of a list")));
-           break;
-
-       case DEEPEND:
+      case BAD_DATA_UNREC:
+         err_mesg = reref(
+            err_mesg, 
+            cons_list(
+               fun,
+               make_static_strnode("doesn\'t like"),
+               error_desc, 
+               make_static_strnode("as input")));
+         break;
+         
+      case DIDNT_OUTPUT:
+          if (didnt_output_name != NIL)
+             {
+             last_call = reref(last_call, didnt_output_name);
+             }
           if (error_desc == NIL)
              {
-             err_mesg = reref(
-                err_mesg,
-                cons_list(make_static_strnode("END inside multi-line instruction.")));
+             error_desc = vref(car(didnt_get_output));
+             ufun = reref(ufun, cadr(didnt_get_output));
+             this_line = reref(this_line, cadr(cdr(didnt_get_output)));
              }
-          else
-             {
-             err_mesg = cons_list(
-                make_static_strnode("END inside multi-line instruction in"),
-                error_desc);
-             }
-             break;
+          err_mesg = reref(
+             err_mesg, 
+             cons_list(
+                last_call,
+                make_static_strnode("didn\'t output to"),
+                error_desc));
+          recoverable = true;
+          break;
+
+      case NOT_ENOUGH:
+         err_mesg = reref(
+            err_mesg, 
+            cons_list(
+               make_static_strnode("not enough inputs to"), 
+               fun));
+           break;
+
+      case BAD_DATA:
+         err_mesg = reref(
+            err_mesg, 
+            cons_list(
+               fun,
+               make_static_strnode("doesn\'t like"), 
+               error_desc,
+               make_static_strnode("as input")));
+         recoverable = true;
+         break;
+
+      case APPLY_BAD_DATA:
+         err_mesg = reref(
+            err_mesg, 
+            cons_list(
+               make_static_strnode("APPLY doesn\'t like"),
+               error_desc,
+               make_static_strnode("as input")));
+         recoverable = true;
+         break;
+
+      case TOO_MUCH:
+         err_mesg = reref(
+            err_mesg,
+            cons_list(make_static_strnode("too much inside ()\'s")));
+         break;
+
+      case DK_WHAT_UP:
+         uplevel = true;
+         // FALLTHROUGH
+      case DK_WHAT:
+         err_mesg = reref(
+            err_mesg,
+            cons_list(
+               make_static_strnode("You don\'t say what to do with"), 
+               error_desc));
+         break;
+
+      case PAREN_MISMATCH:
+         err_mesg = reref(
+            err_mesg,
+            cons_list(make_static_strnode("too many (\'s")));
+         break;
+
+      case NO_VALUE:
+         err_mesg = reref(
+            err_mesg,
+            cons_list(
+               error_desc,
+               make_static_strnode("has no value")));
+         recoverable = true;
+         break;
+
+      case UNEXPECTED_PAREN:
+         err_mesg = reref(
+            err_mesg,
+            cons_list(make_static_strnode("unexpected \')\'")));
+         break;
+
+      case UNEXPECTED_BRACKET:
+         err_mesg = reref(
+            err_mesg,
+            cons_list(make_static_strnode("unexpected \']\'")));
+         break;
+
+      case UNEXPECTED_BRACE:
+         err_mesg = reref(
+            err_mesg,
+            cons_list(make_static_strnode("unexpected \'}\'")));
+         break;
+
+      case DK_HOW:
+         recoverable = true;
+         /* FALLTHROUGH */
+
+      case DK_HOW_UNREC:
+         err_mesg = reref(
+            err_mesg,
+            cons_list(
+               make_static_strnode("I don\'t know how to"), 
+               error_desc));
+         break;
+
+      case NO_CATCH_TAG:
+         err_mesg = reref(
+            err_mesg,
+            cons_list(
+               make_static_strnode("Can't find catch tag for"), 
+               error_desc));
+         break;
+
+      case ALREADY_DEFINED:
+         err_mesg = reref(
+            err_mesg,
+            cons_list(
+               error_desc,
+               make_static_strnode("is already defined")));
+         break;
+
+      case STOP_ERROR:
+         yield_flag = 1;
+         err_mesg = reref(
+            err_mesg,
+            cons_list(make_static_strnode("Stopping...")));
+         break;
+
+      case ALREADY_DRIBBLING:
+         err_mesg = reref(
+            err_mesg, 
+            cons_list(make_static_strnode("Already dribbling")));
+         break;
+
+      case FILE_ERROR:
+         err_mesg = reref(
+            err_mesg,
+            cons_list(
+               make_static_strnode("File system error:"),
+               error_desc));
+         break;
+
+      case IF_WARNING:
+         err_mesg = reref(
+            err_mesg,
+            cons_list(make_static_strnode("Assuming you mean IFELSE, not IF")));
+         warning = true;
+         break;
+
+      case SHADOW_WARN:
+         err_mesg = reref(
+            err_mesg, 
+            cons_list(
+               error_desc,
+               make_static_strnode("shadowed by local in procedure call")));
+         warning = true;
+         break;
+
+      case USER_ERR:
+         if (error_desc == Unbound)
+            {
+            err_mesg = reref(
+               err_mesg,
+               cons_list(make_static_strnode("Throw \"Error")));
+            }
+         else
+            {
+            uplevel = true;
+            if (is_list(error_desc))
+               {
+               err_mesg = reref(err_mesg, error_desc);
+               }
+            else
+               {
+               err_mesg = reref(err_mesg, cons_list(error_desc));
+               }
+            }
+         break;
+         
+      case IS_PRIM:
+         err_mesg = reref(
+            err_mesg,
+            cons_list(
+               error_desc,
+               make_static_strnode("is a primitive")));
+         break;
+
+      case NOT_INSIDE:
+         err_mesg = reref(
+            err_mesg,
+            cons_list(make_static_strnode("Can't use TO inside a procedure")));
+         break;
+         
+      case AT_TOPLEVEL:
+         err_mesg = reref(
+            err_mesg,
+            cons_list(
+               make_static_strnode("Can only use"),
+               error_desc,
+               make_static_strnode("inside a procedure")));
+         break;
+
+      case NO_TEST:
+         err_mesg = reref(
+            err_mesg, 
+            cons_list(
+               fun,
+               make_static_strnode("without TEST")));
+         break;
+
+      case ERR_MACRO:
+         err_mesg = reref(
+            err_mesg,
+            cons_list(
+               make_static_strnode("Macro returned"), 
+               error_desc,
+               make_static_strnode("instead of a list")));
+         break;
+
+      case DEEPEND:
+         if (error_desc == NIL)
+            {
+            err_mesg = reref(
+               err_mesg,
+               cons_list(make_static_strnode("END inside multi-line instruction.")));
+            }
+         else
+            {
+            err_mesg = cons_list(
+               make_static_strnode("END inside multi-line instruction in"),
+               error_desc);
+            }
+         break;
              
-       default:
-           prepare_to_exit(FALSE);
-           printfx("Unknown error condition - internal error.\n");
-           exit(1);
+      default:
+         prepare_to_exit(FALSE);
+         printfx("Unknown error condition - internal error.\n");
+         exit(1);
       }
    deref(error_desc);
    deref(didnt_output_name);
@@ -386,7 +388,9 @@ NODE *err_logo(ERR_TYPES error_type, NODE *error_desc)
       return Unbound;
       }
 
-   err_act = vref(valnode__caseobj(Erract));
+   NODE * err_act = vref(valnode__caseobj(Erract));
+   NODE * val     = Unbound;
+
    if (err_act != NIL && err_act != UNDEFINED)
       {
       if (error_type != erract_errtype)
@@ -534,9 +538,14 @@ NODE *lcontinue(NODE *args)
    stopping_flag = THROWING;
    throw_node = reref(throw_node, Pause);
    if (args != NIL)
+      {
       output_node = reref(output_node, car(args));
+      }
    else
+      {
       output_node = reref(output_node, Unbound);
+      }
+
    return Unbound;
    }
 
