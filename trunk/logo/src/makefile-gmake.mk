8cecc460f132aa3783b82537610ad4f693ca0160
david_costanzo <david_costanzo@9d0050d7-00b0-40f9-871c-8e02cbd5794f>
2006-04-10 01:22:39 +0000

    Create a "clean" target in makefile-gmake.mk that removes the need for the "clean.bat" and "cleanall.bat" files.
    git-svn-id: https://svn.code.sf.net/p/fmslogo/source@1042 9d0050d7-00b0-40f9-871c-8e02cbd5794f

--- trunk/logo/src/makefile-gmake.mk
+++ trunk/logo/src/makefile-gmake.mk
@@ -5,3 +5,19 @@
 all :
 	make
 	tdstrp32 fmslogo.exe
+
+clean :
+	$(RM) $(shell /usr/bin/find release -name '*.obj')
+	$(RM) $(shell /usr/bin/find release -name '*.res')
+	$(RM) $(shell /usr/bin/find -name '*.bak')
+	$(RM) $(shell /usr/bin/find -name '*~')
+	$(RM) fmslogo*.ils
+	$(RM) fmslogo*.ilc
+	$(RM) fmslogo*.ild
+	$(RM) fmslogo*.ilf
+	$(RM) fmslogo*.tds
+	$(RM) logo32*.mrt
+	$(RM) logo32*.mbt
+	$(RM) logo*.csm
+	$(RM) *.fts
+	$(RM) *.gid
