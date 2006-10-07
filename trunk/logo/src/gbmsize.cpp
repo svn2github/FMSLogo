bedc149eff7333789bce847434884a19dea731fc
david_costanzo <david_costanzo@9d0050d7-00b0-40f9-871c-8e02cbd5794f>
2006-10-07 23:55:16 +0000

    Use Extract Function to centralize the common code between the BMP->GIF and GIF->BMP conversion routins.  This makes the code smaller and more maintainable.
    git-svn-id: https://svn.code.sf.net/p/fmslogo/source@1645 9d0050d7-00b0-40f9-871c-8e02cbd5794f

--- trunk/logo/src/gbmsize.cpp
+++ trunk/logo/src/gbmsize.cpp
@@ -1,64 +1,44 @@
-/*
-gbmsize.cpp - Change size of General Bitmap
-*/
+// gbmsize.cpp - Functions for converting BMP to GIF and GIF to BMP
 
 #include <stdio.h>
-#include <ctype.h>
-#include <string.h>
-#include <stddef.h>
 #include <stdlib.h>
-#include <stdarg.h>
-#include <memory.h>
-#include <malloc.h>
-#include <io.h>
 #include <fcntl.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#ifndef O_BINARY
-#define	O_BINARY	0
-#endif
+
 #include "gbm.h"
 
-int gbmBmpToGif(const char *fn_src, const char *fn_dst)
-   {
-   int  w = -1;
-   int  h = -1;
-   char opt_src[] = "";
-   char opt_dst[] = "";
-   int  fd;
-   int  ft_src;
-   int  ft_dst;
-   int  stride;
-   int  flag;
+const int FILETYPE_BMP = 0;
+const int FILETYPE_GIF = 1;
 
-   GBM_ERR rc;
+static
+byte * 
+gbmReadFile(
+   const char * filename, 
+   int          src_filetype,
+   int          dst_filetype, 
+   GBM        & gbm, 
+   GBMRGB     * gbmrgb
+   )
+   {
    GBMFT   gbmft;
-   GBM     gbm;
-   GBMRGB  gbmrgb[0x100];
    byte    *data;
 
-   gbm_init();
-
-   ft_src = 0;
-   ft_dst = 1;
-
-   if ( (fd = gbm_io_open(fn_src, O_RDONLY|O_BINARY)) == -1 )
+   int  fd;
+   if ( (fd = gbm_io_open(filename, O_RDONLY|O_BINARY)) == -1 )
       {
       // fatal("can't open %s", fn_src);
-      return 1;
+      return NULL;
       }
 
-   if ( (rc = gbm_read_header(fn_src, fd, ft_src, &gbm, opt_src)) != GBM_ERR_OK )
+   char opt_src[] = "";
+   if (gbm_read_header(filename, fd, src_filetype, &gbm, opt_src) != GBM_ERR_OK)
       {
       gbm_io_close(fd);
-      //fatal("can't read header of %s: %s", fn_src, gbm_err(rc));
-      return 2;
+      // fatal("can't read header of %s: %s", fn_src, gbm_err(rc));
+      return NULL;
       }
 
-   w = gbm.w;
-   h = gbm.h;
-
-   gbm_query_filetype(ft_dst, &gbmft);
+   int flag;
+   gbm_query_filetype(dst_filetype, &gbmft);
    switch ( gbm.bpp )
       {
       case 24: flag = GBM_FT_W24; break;
@@ -71,33 +51,49 @@ int gbmBmpToGif(const char *fn_src, const char *fn_dst)
       {
       gbm_io_close(fd);
       // fatal("output bitmap format %s does not support writing %d bpp data", gbmft.short_name, gbm.bpp);
-      return 3;
+      return NULL;
       }
 
-   if ( (rc = gbm_read_palette(fd, ft_src, &gbm, gbmrgb)) != GBM_ERR_OK )
+   if (gbm_read_palette(fd, src_filetype, &gbm, gbmrgb) != GBM_ERR_OK)
       {
       gbm_io_close(fd);
       // fatal("can't read palette of %s: %s", fn_src, gbm_err(rc));
-      return 4;
+      return NULL;
       }
 
-   stride = ( ((gbm.w * gbm.bpp + 31)/32) * 4 );
-   if ( (data = (byte *)malloc((size_t) (stride * gbm.h))) == NULL )
+   int stride = ((gbm.w * gbm.bpp + 31)/32) * 4;
+   if ((data = (byte *)malloc(stride * gbm.h)) == NULL)
       {
       gbm_io_close(fd);
       // fatal("out of memory allocating %d bytes for input bitmap", stride * gbm.h);
-      return 5;
+      return NULL;
       }
 
-   if ( (rc = gbm_read_data(fd, ft_src, &gbm, data)) != GBM_ERR_OK )
+   if (gbm_read_data(fd, src_filetype, &gbm, data) != GBM_ERR_OK)
       {
       free(data);
       gbm_io_close(fd);
       //fatal("can't read bitmap data of %s: %s", fn_src, gbm_err(rc));
-      return 6;
+      return NULL;
       }
 
    gbm_io_close(fd);
+
+   return data;
+   }
+
+
+int gbmBmpToGif(const char *fn_src, const char *fn_dst)
+   {
+   GBM     gbm;
+   GBMRGB  gbmrgb[0x100];
+   byte * data = gbmReadFile(fn_src, FILETYPE_BMP, FILETYPE_GIF, gbm, gbmrgb);
+   if (data == NULL)
+      {
+      return 1;
+      }
+
+   int fd;
 
    extern int bAppendMode;
 
@@ -121,10 +117,8 @@ int gbmBmpToGif(const char *fn_src, const char *fn_dst)
          }
       }
 
-   gbm.w = w;
-   gbm.h = h;
-
-   if ( (rc = gbm_write(fn_dst, fd, ft_dst, &gbm, gbmrgb, data, opt_dst)) != GBM_ERR_OK )
+   char opt_dst[] = "";
+   if (gbm_write(fn_dst, fd, FILETYPE_GIF, &gbm, gbmrgb, data, opt_dst) != GBM_ERR_OK)
       {
       gbm_io_close(fd);
       remove(fn_dst);
@@ -134,93 +128,24 @@ int gbmBmpToGif(const char *fn_src, const char *fn_dst)
       }
 
    gbm_io_close(fd);
-   free(data);
 
-   gbm_deinit();
+   free(data);
 
    return 0;
    }
 
 int gbmGifToBmp(const char *fn_src, const char *fn_dst)
    {
-   int  w = -1;
-   int  h = -1;
-   char opt_src[] = "";
-   char opt_dst[] = "";
-   int  fd;
-   int  ft_src;
-   int  ft_dst;
-   int  stride;
-   int  flag;
-
-   GBM_ERR rc;
-   GBMFT   gbmft;
-   GBM     gbm;
-   GBMRGB  gbmrgb[0x100];
-   byte    *data;
-
-   gbm_init();
-
-   ft_src = 1;
-   ft_dst = 0;
-
-   if ( (fd = gbm_io_open(fn_src, O_RDONLY|O_BINARY)) == -1 )
+   GBM       gbm;
+   GBMRGB    gbmrgb[0x100];
+   byte * data = gbmReadFile(fn_src, FILETYPE_GIF, FILETYPE_BMP, gbm, gbmrgb);
+   if (data == NULL)
       {
-      // fatal("can't open %s", fn_src);
       return 1;
       }
 
-   if ( (rc = gbm_read_header(fn_src, fd, ft_src, &gbm, opt_src)) != GBM_ERR_OK )
-      {
-      gbm_io_close(fd);
-      // fatal("can't read header of %s: %s", fn_src, gbm_err(rc));
-      return 2;
-      }
 
-   w = gbm.w;
-   h = gbm.h;
-
-   gbm_query_filetype(ft_dst, &gbmft);
-   switch ( gbm.bpp )
-      {
-      case 24: flag = GBM_FT_W24; break;
-      case 8:  flag = GBM_FT_W8;  break;
-      case 4:  flag = GBM_FT_W4;  break;
-      case 1:  flag = GBM_FT_W1;  break;
-      }
-
-   if ( (gbmft.flags & flag) == 0 )
-      {
-      gbm_io_close(fd);
-      //fatal("output bitmap format %s does not support writing %d bpp data", gbmft.short_name, gbm.bpp);
-      return 3;
-      }
-
-   if ( (rc = gbm_read_palette(fd, ft_src, &gbm, gbmrgb)) != GBM_ERR_OK )
-      {
-      gbm_io_close(fd);
-      //fatal("can't read palette of %s: %s", fn_src, gbm_err(rc));
-      return 4;
-      }
-
-   stride = ( ((gbm.w * gbm.bpp + 31)/32) * 4 );
-   if ( (data = (byte *)malloc((size_t) (stride * gbm.h))) == NULL )
-      {
-      gbm_io_close(fd);
-      //fatal("out of memory allocating %d bytes for input bitmap", stride * gbm.h);
-      return 5;
-      }
-   
-   if ( (rc = gbm_read_data(fd, ft_src, &gbm, data)) != GBM_ERR_OK )
-      {
-      free(data);
-      gbm_io_close(fd);
-      //fatal("can't read bitmap data of %s: %s", fn_src, gbm_err(rc));
-      return 6;
-      }
-   
-   gbm_io_close(fd);
-
+   int fd;
    if ( (fd = gbm_io_create(fn_dst, O_WRONLY|O_BINARY)) == -1 )
       {
       free(data);
@@ -228,10 +153,8 @@ int gbmGifToBmp(const char *fn_src, const char *fn_dst)
       return 7;
       }
    
-   gbm.w = w;
-   gbm.h = h;
-
-   if ( (rc = gbm_write(fn_dst, fd, ft_dst, &gbm, gbmrgb, data, opt_dst)) != GBM_ERR_OK )
+   char opt_dst[] = "";
+   if (gbm_write(fn_dst, fd, FILETYPE_BMP, &gbm, gbmrgb, data, opt_dst) != GBM_ERR_OK)
       {
       gbm_io_close(fd);
       remove(fn_dst);
@@ -242,8 +165,6 @@ int gbmGifToBmp(const char *fn_src, const char *fn_dst)
 
    gbm_io_close(fd);
    free(data);
-
-   gbm_deinit();
 
    return 0;
    }
