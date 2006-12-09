4f31f21b54a9c3596b602a8819d42fa872cd7fc1
david_costanzo <david_costanzo@9d0050d7-00b0-40f9-871c-8e02cbd5794f>
2006-12-09 19:40:41 +0000

    Change mklocale.pl to know the names of all supported locales so it doesn't need to take command-line parameters, anymore.
    git-svn-id: https://svn.code.sf.net/p/fmslogo/source@1901 9d0050d7-00b0-40f9-871c-8e02cbd5794f

--- trunk/logo/src/mklocale.pl
+++ trunk/logo/src/mklocale.pl
@@ -1,62 +1,64 @@
 use strict;
 use IO::File;
 
-if (@ARGV != 2) {
-  print "Usage: $0 country_code locale_name\n";
-  exit 1;
-}
+sub UpdateLocalizationFiles($$) {
 
-my $newLocale  = $ARGV[0];
-my $localeName = $ARGV[1];
+  my $CountryCode = shift or die "too few arguments";
+  my $LocaleName  = shift or die "too few arguments";
 
-my %translations = ();
+  my %translations = ();
 
-#
-# Build a table of everything that has already been translated
-#
-my $existingStringTable = new IO::File "localizedstrings-$newLocale.h";
-if ($existingStringTable) {
+  #
+  # Build a table of everything that has already been translated
+  #
+  my $existingStringTable = new IO::File "localizedstrings-$CountryCode.h";
+  if ($existingStringTable) {
 
-  while (<$existingStringTable>) {
-    chomp;
-    if (m/^\#define\s+([\w\d_]+)\s*(.*)$/) {
-      $translations{$1} = $2;
+    while (<$existingStringTable>) {
+      chomp;
+      if (m/^\#define\s+([\w\d_]+)\s*(.*)$/) {
+        $translations{$1} = $2;
+      }
     }
+    $existingStringTable->close();
   }
-  $existingStringTable->close();
-}
 
-my $newFile = new IO::File "> localizedstrings-$newLocale.h" or die $!;
+  my $newFile = new IO::File "> localizedstrings-$CountryCode.h" or die $!;
 
 
-#
-# Write out the new localization using the English one as a basis
-#
-my $original = new IO::File "localizedstrings-en.h" or die $!;
-while (<$original>) {
-  chomp;
-  my $line = $_;
+  #
+  # Write out the new localization using the English one as a basis
+  #
+  my $original = new IO::File "localizedstrings-en.h" or die $!;
+  while (<$original>) {
+    chomp;
+    my $line = $_;
 
-  if ($line =~ m/^\#define\s+([\w\d_]+)(\s*).*$/) {
-    if ($translations{$1}) {
-      # this has already been localized
-      print $newFile "#define $1$2$translations{$1}\n";
+    if ($line =~ m/^\#define\s+([\w\d_]+)(\s*).*$/) {
+      if ($translations{$1}) {
+        # this has already been localized
+        print $newFile "#define $1$2$translations{$1}\n";
+      }
+      else {
+        # this has not yet been translated
+        print $newFile "$line // NOT_YET_LOCALIZED\n";
+      }
+    }
+    elsif ($line =~ m/localized strings for English/) {
+      # translate the locale English
+      $line =~ s/English/$LocaleName/;
+      print $newFile "$line\n";
     }
     else {
-      # this has not yet been translated
-      print $newFile "$line // NOT_YET_LOCALIZED\n";
+      # this line is not localizable
+      print $newFile "$line\n";
     }
   }
-  elsif ($line =~ m/localized strings for English/) {
-    # translate the locale English
-    $line =~ s/English/$localeName/;
-    print $newFile "$line\n";
-  }
-  else {
-    # this line is not localizable
-    print $newFile "$line\n";
-  }
+
+  $original->close();
+  $newFile->close();
 }
 
-$original->close();
-$newFile->close();
+
+UpdateLocalizationFiles('fr', 'French');
+UpdateLocalizationFiles('gr', 'Greek');
