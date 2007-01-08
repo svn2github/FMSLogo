bbf5b73ac650686652f444e816ab1686769bbf06
david_costanzo <david_costanzo@9d0050d7-00b0-40f9-871c-8e02cbd5794f>
2007-01-08 08:10:51 +0000

    Fix bug #1624729: FLONUM_EPSILON is too small for 3D graphics.
    This fixes Mike Sandy's polygon.lgo program by making the turtle's orientation "snap to" value like 0, 90, 180, 270 when it is close.  This is partly a work-around for bug #1429938 (SETORIENTATION does not set the ORIENTATION exactly).
    
    
    git-svn-id: https://svn.code.sf.net/p/fmslogo/source@2067 9d0050d7-00b0-40f9-871c-8e02cbd5794f

--- trunk/logo/src/logocore.h
+++ trunk/logo/src/logocore.h
@@ -194,7 +194,7 @@ typedef long    FIXNUM;
 
 typedef double FLONUM;
 #define FLONUM_MAX     DBL_MAX
-#define FLONUM_EPSILON DBL_EPSILON
+#define FLONUM_EPSILON 1.0e-12 // don't use DLB_EPSILON (see bug #1624729)
 
 struct NODE
    {
