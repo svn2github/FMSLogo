697e0060e5a5bca0418aca495022439755a47408
david_costanzo <david_costanzo@9d0050d7-00b0-40f9-871c-8e02cbd5794f>
2007-08-25 03:49:02 +0000

    retabbify with 4 spaces per indentation.
    git-svn-id: https://svn.code.sf.net/p/fmslogo/source@2354 9d0050d7-00b0-40f9-871c-8e02cbd5794f

--- trunk/logo/src/gbm.h
+++ trunk/logo/src/gbm.h
@@ -56,65 +56,65 @@ palette), gbm_read_data is additionally allowed to follow a gbm_read_header.
 */
 
 #ifndef GBM_H
-#define	GBM_H
+#define GBM_H
 
 #ifndef BOOLEAN_DEFINED
-#define	BOOLEAN_DEFINED
-typedef	int BOOLEAN;
-#define	TRUE  1
-#define	FALSE 0
+#define BOOLEAN_DEFINED
+typedef int BOOLEAN;
+#define TRUE  1
+#define FALSE 0
 #endif
 
 #ifndef BASICTYPES_DEFINED
-#define	BASICTYPES_DEFINED
+#define BASICTYPES_DEFINED
 typedef unsigned  char  byte;
 typedef unsigned short  word;
 typedef unsigned  long dword;
 #endif
 
 typedef int GBM_ERR;
-#define	GBM_ERR_OK		((GBM_ERR) 0)
-#define	GBM_ERR_MEM		((GBM_ERR) 1)
-#define	GBM_ERR_NOT_SUPP	((GBM_ERR) 2)
-#define	GBM_ERR_BAD_OPTION	((GBM_ERR) 3)
-#define	GBM_ERR_NOT_FOUND	((GBM_ERR) 4)
-#define	GBM_ERR_BAD_MAGIC	((GBM_ERR) 5)
-#define	GBM_ERR_BAD_SIZE	((GBM_ERR) 6)
-#define	GBM_ERR_READ		((GBM_ERR) 7)
-#define	GBM_ERR_WRITE		((GBM_ERR) 8)
-#define	GBM_ERR_BAD_ARG		((GBM_ERR) 9)
+#define GBM_ERR_OK              ((GBM_ERR) 0)
+#define GBM_ERR_MEM             ((GBM_ERR) 1)
+#define GBM_ERR_NOT_SUPP        ((GBM_ERR) 2)
+#define GBM_ERR_BAD_OPTION      ((GBM_ERR) 3)
+#define GBM_ERR_NOT_FOUND       ((GBM_ERR) 4)
+#define GBM_ERR_BAD_MAGIC       ((GBM_ERR) 5)
+#define GBM_ERR_BAD_SIZE        ((GBM_ERR) 6)
+#define GBM_ERR_READ            ((GBM_ERR) 7)
+#define GBM_ERR_WRITE           ((GBM_ERR) 8)
+#define GBM_ERR_BAD_ARG         ((GBM_ERR) 9)
 
-#define	GBM_FT_R1		0x0001
-#define	GBM_FT_R4		0x0002
-#define	GBM_FT_R8		0x0004
-#define	GBM_FT_R24		0x0008
-#define	GBM_FT_W1		0x0010
-#define	GBM_FT_W4		0x0020
-#define	GBM_FT_W8		0x0040
-#define	GBM_FT_W24		0x0080
+#define GBM_FT_R1               0x0001
+#define GBM_FT_R4               0x0002
+#define GBM_FT_R8               0x0004
+#define GBM_FT_R24              0x0008
+#define GBM_FT_W1               0x0010
+#define GBM_FT_W4               0x0020
+#define GBM_FT_W8               0x0040
+#define GBM_FT_W24              0x0080
 
 struct GBMFT
-   {
-   char *short_name;		/* Eg: "Targa"                       */
-   char *long_name;		/* Eg: "Truevision Targa / Vista"    */
-   char *extensions;		/* Eg: "TGA VST"                     */
-   int flags;			/* What functionality exists         */
-   };
+{
+    char *short_name;           /* Eg: "Targa"                       */
+    char *long_name;            /* Eg: "Truevision Targa / Vista"    */
+    char *extensions;           /* Eg: "TGA VST"                     */
+    int flags;                  /* What functionality exists         */
+};
 
 struct GBMRGB { byte r, g, b; };
 
-#define	PRIV_SIZE 2000
+#define PRIV_SIZE 2000
 
 struct GBM
-   {
-   int w, h, bpp;			/* Bitmap dimensions                 */
-   byte priv[PRIV_SIZE];		/* Private internal buffer           */
-   };
+{
+    int w, h, bpp;              /* Bitmap dimensions                 */
+    byte priv[PRIV_SIZE];       /* Private internal buffer           */
+};
 
 #ifndef _GBM_
 
-#define	GBMEXPORT
-#define	GBMENTRY
+#define GBMEXPORT
+#define GBMENTRY
 
 GBMEXPORT int  GBMENTRY gbm_io_open  (const char *fn, int mode);
 GBMEXPORT int  GBMENTRY gbm_io_create(const char *fn, int mode);
@@ -135,4 +135,3 @@ GBMEXPORT GBM_ERR GBMENTRY gbm_write(const char *fn, int fd, int ft, const GBM *
 #endif
 
 #endif
-
