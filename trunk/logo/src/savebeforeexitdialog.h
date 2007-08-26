c0bc2593fc38636f9f43b4c24855ac3f8933756e
david_costanzo <david_costanzo@9d0050d7-00b0-40f9-871c-8e02cbd5794f>
2007-08-26 01:45:54 +0000

    Insert "// -*- c++ -*-" on the first line so that XEmacs knows to intent the file as C++ (in particular, that the access labels should go flush left).
    git-svn-id: https://svn.code.sf.net/p/fmslogo/source@2369 9d0050d7-00b0-40f9-871c-8e02cbd5794f

--- trunk/logo/src/savebeforeexitdialog.h
+++ trunk/logo/src/savebeforeexitdialog.h
@@ -1,45 +1,42 @@
-/*
-*  Copyright (C) 2006 by David Costanzo
-*
-*  This program is free software; you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation; either version 2 of the License, or
-*  (at your option) any later version.
-*
-*  This program is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
-*
-*  You should have received a copy of the GNU General Public License
-*  along with this program; if not, write to the Free Software
-*  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-*
-*/
+// -*- c++ -*-
+//
+//  Copyright (C) 2006 by David Costanzo
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
 #ifndef _SAVEBEFOREEXITDIALOG_H_
 #define _SAVEBEFOREEXITDIALOG_H_
 
 class CSaveBeforeExitDialog : public TDialog
-   {
- public:
+{
+public:
 
-   CSaveBeforeExitDialog(TWindow * Parent);
-   int GetExitCode();
+    CSaveBeforeExitDialog(TWindow * Parent);
+    int GetExitCode();
 
- protected:
-   void SetupWindow();
+protected:
+    void SetupWindow();
 
- private:
+private:
 
-   void EvSaveBeforeExit();
-   void EvExitWithoutSaving();
+    void EvSaveBeforeExit();
+    void EvExitWithoutSaving();
 
-   int m_ExitStatus;
+    int m_ExitStatus;
 
-   DECLARE_RESPONSE_TABLE(CSaveBeforeExitDialog);
-   }
-;
-
+    DECLARE_RESPONSE_TABLE(CSaveBeforeExitDialog);
+};
 
 #endif // _SAVEBEFOREEXITDIALOG_H_
