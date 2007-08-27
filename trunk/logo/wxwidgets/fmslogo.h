56b176f687e1a44377eb8af5e85a5069ac742f26
david_costanzo <david_costanzo@9d0050d7-00b0-40f9-871c-8e02cbd5794f>
2007-08-27 06:52:00 +0000

    Add the main menu.
    git-svn-id: https://svn.code.sf.net/p/fmslogo/source@2379 9d0050d7-00b0-40f9-871c-8e02cbd5794f

---
+++ trunk/logo/wxwidgets/fmslogo.h
@@ -0,0 +1,3 @@
+const int DEFAULT_COMMANDER_HEIGHT = 150;
+const int MIN_COMMANDER_HEIGHT     = 100;
+const int DEFAULT_SPLITTER_WIDTH   = 5;
--- trunk/logo/wxwidgets/mainframe.cpp
+++ trunk/logo/wxwidgets/mainframe.cpp
@@ -26,7 +26,9 @@
 #include "wx/dcmirror.h"
 
 #include "commander.h"
+#include "logocore.h"
 #include "localizedstrings.h"
+#include "fmslogo.h"
 
 // ----------------------------------------------------------------------------
 // constants
@@ -163,6 +165,107 @@ BEGIN_EVENT_TABLE(CMainFrame, wxFrame)
     EVT_UPDATE_UI(SPLIT_UNSPLIT, CMainFrame::UpdateUIUnsplit)
 END_EVENT_TABLE()
 
+struct MENUITEM 
+{
+    const char *  MenuText;
+    int           MenuId;
+};
+
+static
+void
+FillMenu(
+    wxMenu *          Menu,
+    const MENUITEM *  MenuItems,
+    size_t            MenuItemsLength
+    )
+{
+    for (size_t i = 0; i < MenuItemsLength; i++)
+    {
+        if (MenuItems[i].MenuText != NULL)
+        {
+            Menu->Append(
+                MenuItems[i].MenuId,
+                MenuItems[i].MenuText);
+        }
+        else
+        {
+            Menu->AppendSeparator();
+        }
+    }
+}
+
+static
+void
+AppendChildMenu(
+    wxMenuBar *       MainMenu,
+    const char *      ChildMenuText,
+    const MENUITEM *  ChildMenuItems,
+    size_t            ChildMenuItemsLength
+    )
+{
+    // create the popup menu
+    wxMenu * childMenu = new wxMenu;
+
+    // fill the child menu with its items
+    FillMenu(childMenu, ChildMenuItems, ChildMenuItemsLength);
+
+    // append the child menu to the main menu
+    MainMenu->Append(childMenu, ChildMenuText);
+}
+
+enum MenuIds
+{
+    ID_FILENEW,
+    ID_FILELOAD,
+    ID_FILEOPEN,
+    ID_FILESAVE,
+    ID_FILESAVEAS,
+    ID_FILEEDIT,
+    ID_FILEERASE,
+    ID_EXIT,
+
+    ID_BITMAPNEW,
+    ID_BITMAPOPEN,
+    ID_BITMAPSAVE,
+    ID_BITMAPSAVEAS,
+
+    ID_BITMAPPRINT,
+    ID_BITMAPPRINTERSETUP,
+
+    ID_BITMAPPRINTERAREA,
+
+    ID_SETPENSIZE,
+
+    ID_SETFONT,
+    ID_SETCOMMANDERFONT,
+
+    ID_SETPENCOLOR,
+    ID_SETFLOODCOLOR,
+    ID_SETSCREENCOLOR,
+
+    ID_ZOOMIN,
+    ID_ZOOMOUT,
+    ID_ZOOMNORMAL,
+
+    ID_HELP,
+#if LOCALE!=1033
+    // options for translating to/from English
+    ID_HELPLANGTOENGLISH,
+    ID_HELPENGLISHTOLANG,
+#endif
+    ID_HELPMCI,
+    ID_HELPHELP,
+
+    ID_HELPTUTORIAL,
+    ID_HELPDEMO,
+    ID_HELPEXAMPLES,
+    ID_HELPRELEASENOTES,
+
+    ID_HELPABOUT,
+    ID_HELPABOUTMS,
+};
+
+
 // My frame constructor
 CMainFrame::CMainFrame()
        : wxFrame(NULL, wxID_ANY, _T("wxSplitterWindow sample"),
@@ -173,41 +276,82 @@ CMainFrame::CMainFrame()
     CreateStatusBar(2);
 #endif // wxUSE_STATUSBAR
 
+    //
+    // Construct the main menu
+    //
+    static const MENUITEM fileMenuItems[] = {
+        {LOCALIZED_FILE_NEW,    ID_FILENEW},
+        {LOCALIZED_FILE_LOAD,   ID_FILELOAD},
+        {LOCALIZED_FILE_OPEN,   ID_FILEOPEN},
+        {LOCALIZED_FILE_SAVE,   ID_FILESAVE},
+        {LOCALIZED_FILE_SAVEAS, ID_FILESAVEAS},
+        {0},
+        {LOCALIZED_FILE_EDIT,   ID_FILEEDIT},
+        {LOCALIZED_FILE_ERASE,  ID_FILEERASE},
+        {0},
+        {LOCALIZED_FILE_EXIT,   ID_EXIT},
+    };
+
+    static const MENUITEM bitmapMenuItems[] = {
+        {LOCALIZED_BITMAP_NEW,           ID_BITMAPNEW},
+        {LOCALIZED_BITMAP_LOAD,          ID_BITMAPOPEN},
+        {LOCALIZED_BITMAP_SAVE,          ID_BITMAPSAVE},
+        {LOCALIZED_BITMAP_SAVEAS,        ID_BITMAPSAVEAS},
+        {0},
+        {LOCALIZED_BITMAP_PRINT,         ID_BITMAPPRINT},
+        {LOCALIZED_BITMAP_PRINTERSETUP,  ID_BITMAPPRINTERSETUP},
+        {0},
+        {LOCALIZED_BITMAP_ACTIVEAREA,    ID_BITMAPPRINTERAREA},
+    };
+
+    static const MENUITEM setMenuItems[] = {
+        {LOCALIZED_SET_PENSIZE,       ID_SETPENSIZE},
+        {0},
+        {LOCALIZED_SET_LABELFONT,     ID_SETFONT},
+        {LOCALIZED_SET_COMMANDERFONT, ID_SETCOMMANDERFONT},
+        {0},
+        {LOCALIZED_SET_PENCOLOR,      ID_SETPENCOLOR},
+        {LOCALIZED_SET_FLOODCOLOR,    ID_SETFLOODCOLOR},
+        {LOCALIZED_SET_SCREENCOLOR,   ID_SETSCREENCOLOR},
+    };
+
+    static const MENUITEM zoomMenuItems[] = {
+        {LOCALIZED_ZOOM_IN,     ID_ZOOMIN},
+        {LOCALIZED_ZOOM_OUT,    ID_ZOOMOUT},
+        {LOCALIZED_ZOOM_NORMAL, ID_ZOOMNORMAL},
+    };
+ 
+    static const MENUITEM helpMenuItems[] = {
+        {LOCALIZED_HELP_INDEX,         ID_HELP},
+#if LOCALE!=1033
+        // options for translating to/from English
+        {LOCALIZED_HELP_LANGTOENGLISH, ID_HELPLANGTOENGLISH},
+        {LOCALIZED_HELP_ENGLISHTOLANG, ID_HELPENGLISHTOLANG},
+#endif
+        {LOCALIZED_HELP_MCI,           ID_HELPMCI},
+        {LOCALIZED_HELP_HELP,          ID_HELPHELP},
+        {0},
+        {LOCALIZED_HELP_TUTORIAL,      ID_HELPTUTORIAL},
+        {LOCALIZED_HELP_DEMO,          ID_HELPDEMO},
+        {LOCALIZED_HELP_EXAMPLE,       ID_HELPEXAMPLES},
+        {LOCALIZED_HELP_RELEASENOTES,  ID_HELPRELEASENOTES},
+        {0},
+        {LOCALIZED_HELP_ABOUTFMSLOGO,  ID_HELPABOUT},
+        {LOCALIZED_HELP_MS,            ID_HELPABOUTMS},
+    };
+
     // Make a menubar
-    wxMenu *splitMenu = new wxMenu;
+    wxMenuBar * mainMenu = new wxMenuBar;
 
-    splitMenu->Append(SPLIT_UNSPLIT,
-                      _T("&Unsplit\tCtrl-U"),
-                      _T("Unsplit"));
-    splitMenu->AppendSeparator();
+    AppendChildMenu(mainMenu, LOCALIZED_FILE,   fileMenuItems,   ARRAYSIZE(fileMenuItems));
+    AppendChildMenu(mainMenu, LOCALIZED_BITMAP, bitmapMenuItems, ARRAYSIZE(bitmapMenuItems));
+    AppendChildMenu(mainMenu, LOCALIZED_SET,    setMenuItems,    ARRAYSIZE(setMenuItems));
+    AppendChildMenu(mainMenu, LOCALIZED_ZOOM,   zoomMenuItems,   ARRAYSIZE(zoomMenuItems));
+    AppendChildMenu(mainMenu, LOCALIZED_HELP,   helpMenuItems,   ARRAYSIZE(helpMenuItems));
 
-    splitMenu->AppendCheckItem(SPLIT_LIVE,
-                               _T("&Live update\tCtrl-L"),
-                               _T("Toggle live update mode"));
-    splitMenu->Append(SPLIT_SETPOSITION,
-                      _T("Set splitter &position\tCtrl-P"),
-                      _T("Set the splitter position"));
-    splitMenu->Append(SPLIT_SETMINSIZE,
-                      _T("Set &min size\tCtrl-M"),
-                      _T("Set minimum pane size"));
-    splitMenu->Append(SPLIT_SETGRAVITY,
-                      _T("Set &gravity\tCtrl-G"),
-                      _T("Set gravity of sash"));
-    splitMenu->AppendSeparator();
+    SetMenuBar(mainMenu);
 
-    splitMenu->Append(SPLIT_REPLACE,
-                      _T("&Replace right window"),
-                      _T("Replace right window"));
-    splitMenu->AppendSeparator();
 
-    splitMenu->Append(SPLIT_QUIT, _T("E&xit\tAlt-X"), _T("Exit"));
-
-    wxMenuBar *menuBar = new wxMenuBar;
-    menuBar->Append(splitMenu, _T("&Splitter"));
-
-    SetMenuBar(menuBar);
-
-    menuBar->Check(SPLIT_LIVE, true);
     m_splitter = new MySplitterWindow(this);
     
     m_splitter->SetSashGravity(1.0);
@@ -367,6 +511,8 @@ MySplitterWindow::MySplitterWindow(wxFrame *parent)
                                    wxSP_3D | wxSP_LIVE_UPDATE |
                                    wxCLIP_CHILDREN /* | wxSP_NO_XP_THEME */ )
 {
+    SetMinimumPaneSize(MIN_COMMANDER_HEIGHT);
+
     m_frame = parent;
 }
 
