fe400b3cf888956331fb7dd43bc69da61ea41a4f
david_costanzo <david_costanzo@9d0050d7-00b0-40f9-871c-8e02cbd5794f>
2009-06-16 17:52:42 +0000

    Add Scintilla v 178 into the FMSLogo build.  Some customizations have been made to make it work with FMSLogo, including some build tweaking and replacing hard-coded strings with internationalized strings.
    git-svn-id: https://svn.code.sf.net/p/fmslogo/source@2843 9d0050d7-00b0-40f9-871c-8e02cbd5794f

---
+++ trunk/logo/src/scintilla/include/BaseTsd.h
@@ -0,0 +1,4 @@
+
+#define WINAPI __stdcall
+#define SIZE_T size_t
+#define DllMain DllEntryPoint
---
+++ trunk/logo/src/scintilla/include/FMSlogoSupplemental.h
@@ -0,0 +1,8 @@
+
+typedef struct _BLENDFUNCTION
+{
+    unsigned char BlendOp;
+    unsigned char BlendFlags;
+    unsigned char SourceConstantAlpha;
+    unsigned char AlphaFormat;
+}BLENDFUNCTION,*PBLENDFUNCTION;
