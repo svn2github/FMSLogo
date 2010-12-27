d5dc02ddc91a209a5f4c718fbc52802128226854
david_costanzo <david_costanzo@9d0050d7-00b0-40f9-871c-8e02cbd5794f>
2010-12-27 20:47:59 +0000

    Fix bug #3146515, which is a stack buffer overflow in PO when printing a procedure with a long line.
    git-svn-id: https://svn.code.sf.net/p/fmslogo/source@3052 9d0050d7-00b0-40f9-871c-8e02cbd5794f

--- trunk/logo/src/wrksp.cpp
+++ trunk/logo/src/wrksp.cpp
@@ -1136,16 +1136,29 @@ NODE *po_helper(NODE *arg, int just_titles)  /* >0 for POT, <0 for EDIT       */
                         char *str = expand_slash(currentline);
                         if (g_Writer.GetStream() == stdout)
                         {
-                            // str doesn't end in a newline.  printfx() always
-                            // writes a newline to the commander history,
-                            // regardless of if the string ends in a newline
-                            // (in eseence, the newlineis optional).  However,
-                            // it doesn't write anything whengiven the empty
-                            // string, which is what happens when there's a
-                            // blank line in the procedure definition.  To 
-                            // ensure that blank lines are preserved, we append
-                            // a newline.
-                            printfx("%s\n", str);
+                            if (str[0] == '\0')
+                            {
+                                // str doesn't end in a newline.  
+                                // printfx() always writes a newline to the
+                                // commander history, regardless of if the 
+                                // string ends in a newline (in essence, the 
+                                // newline is optional).  However, it doesn't
+                                // write anything when given the empty
+                                // string, which is what happens when there's a
+                                // blank line in the procedure definition.
+                                // To ensure that blank lines are preserved, we
+                                // write a newline.
+                                // Note that we can't just call 
+                                //   printfx("%s\n", str);
+                                // because that could overflow the buffer in
+                                // printfx()
+                                printfx("\n");
+                            }
+                            else
+                            {
+                                printfx(str);
+                            }
+
                             if (dribblestream != NULL)
                             {
                                 fprintf(dribblestream, "%s\n", str);
