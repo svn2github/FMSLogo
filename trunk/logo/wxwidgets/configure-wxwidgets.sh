e05d8b545635a645970dddb01d11b8835164c9c3
david_costanzo <david_costanzo@9d0050d7-00b0-40f9-871c-8e02cbd5794f>
2012-04-23 19:54:37 +0000

    Switch the wxWidgets project from using the cygwin compilers to using the MingGW cross-compilers that come with Cygwin 1.7.  This solves two problems.  First, it removes the dependency on cygwin1.dll.  Second, it makes it so that fopen() can convert the CRLF line endings to LF, so that Logo files can be properly understood by the parser.
    git-svn-id: https://svn.code.sf.net/p/fmslogo/source@3099 9d0050d7-00b0-40f9-871c-8e02cbd5794f

--- trunk/logo/wxwidgets/configure-wxwidgets.sh
+++ trunk/logo/wxwidgets/configure-wxwidgets.sh
@@ -4,11 +4,12 @@
 # This file is a reminder of how I built wxwidgets on Windows.
 #
 # Use the script like:
+#   bash
 #   cd <wxwidgets_top_dir>
-#   bash <path_to_this_script>/configure-wxwidgets.sh
-#   gmake all
-#   gmake -D contrib all
+#   <path_to_this_script>/configure-wxwidgets.sh
+#   gmake
+#   gmake -C contrib
 
 export PATH=/usr/bin:$PATH
 export MAKE=gmake
-./configure --with-msw --with-expat=builtin --enable-debug_gdb --enable-debug-flag --enable-monolithic --disable-shared --disable-optimise --with-libstc --with-libstl --with-libjpeg=no
+./configure --with-msw --with-expat=builtin --enable-debug_gdb --enable-debug-flag --enable-monolithic --disable-shared --disable-optimise --disable-sockets --with-libstc --with-libstl --with-libjpeg=no --host=i686-pc-mingw32
