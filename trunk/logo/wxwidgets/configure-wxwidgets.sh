e9f67ddea60b5db6c11d86cc3d96b4d3aab22a2a
david_costanzo <david_costanzo@9d0050d7-00b0-40f9-871c-8e02cbd5794f>
2012-04-24 04:47:08 +0000

    Include a note as to why the resulting fmslogo.exe won't run on my windows 95 machine, so that I don't waste time investigating the failure again.
    git-svn-id: https://svn.code.sf.net/p/fmslogo/source@3101 9d0050d7-00b0-40f9-871c-8e02cbd5794f

--- trunk/logo/wxwidgets/configure-wxwidgets.sh
+++ trunk/logo/wxwidgets/configure-wxwidgets.sh
@@ -9,6 +9,18 @@
 #   <path_to_this_script>/configure-wxwidgets.sh
 #   gmake
 #   gmake -C contrib
+#
+#
+# Note that these configuration options will not build an executable that is compatible
+# with Windows 95.  More specifically, the i686 MinGW runtime uses a CMOVNBE instruction
+# in a static initializer.  CMOVcc is only supported on Pentium Pro CPUs and higher.
+# Since Windows 95 is supported on lower CPUs, this can potentially cause FMSLogo to
+# execute an illegal instruction.  For reference, the opcode is
+#
+#   0x0F 0x48
+#
+# To address this, the code must be compiled with a --host that uses a GCC from an older
+# version of MinGW (3.4.2 seems to work).
 
 export PATH=/usr/bin:$PATH
 export MAKE=gmake
