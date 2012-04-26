6269fc607e275fdb4e1bc2da98142851edef54a1
david_costanzo <david_costanzo@9d0050d7-00b0-40f9-871c-8e02cbd5794f>
2012-04-26 02:05:18 +0000

    Codify the steps for building a release version of the wxWidgets fmslogo, including a release version of wxWidgets.
    git-svn-id: https://svn.code.sf.net/p/fmslogo/source@3104 9d0050d7-00b0-40f9-871c-8e02cbd5794f

--- trunk/logo/wxwidgets/configure-wxwidgets.sh
+++ trunk/logo/wxwidgets/configure-wxwidgets.sh
@@ -5,10 +5,11 @@
 #
 # Use the script like:
 #   bash
+#   rm configarg.cache
 #   cd <wxwidgets_top_dir>
 #   <path_to_this_script>/configure-wxwidgets.sh
-#   gmake
-#   gmake -C contrib
+#   gmake clean all
+#   gmake -C contrib/src/stc clean all
 #
 #
 # Note that these configuration options will not build an executable that is compatible
@@ -24,4 +25,35 @@
 
 export PATH=/usr/bin:$PATH
 export MAKE=gmake
-./configure --with-msw --with-expat=builtin --enable-debug_gdb --enable-debug-flag --enable-monolithic --disable-shared --disable-optimise --disable-sockets --with-libstc --with-libstl --with-libjpeg=no --host=i686-pc-mingw32
+
+if [ "$DEBUG" == "1" ];
+then
+  export ADDITIONAL_OPTIONS="--enable-debug --enable-debug_gdb --enable-debug_info --disable-optimise"
+else
+  export ADDITIONAL_OPTIONS="--disable-debug --enable-optimise"
+fi
+
+./configure                \
+    $ADDITIONAL_OPTIONS    \
+    --with-msw             \
+    --with-expat=builtin   \
+    --enable-monolithic    \
+    --disable-shared       \
+    --disable-sockets      \
+    --disable-protocols    \
+    --disable-ftp          \
+    --disable-http         \
+    --disable-tarstream    \
+    --disable-mdi          \
+    --disable-tipdlg       \
+    --disable-largefile    \
+    --disable-tga          \
+    --disable-pcx          \
+    --disable-iff          \
+    --disable-pnm          \
+    --disable-wizarddlg    \
+    --with-libstc          \
+    --with-libstl          \
+    --with-libjpeg=no      \
+    --enable-no_deps       \
+    --host=i686-pc-mingw32 \
