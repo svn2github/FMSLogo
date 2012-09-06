175f08a0b6d2244f1375d4a7fd4dbbcf09406b8a
david_costanzo <david_costanzo@9d0050d7-00b0-40f9-871c-8e02cbd5794f>
2012-09-06 17:46:30 +0000

    Add -march=i586 to prevent the compiler from generating CMOVcc instructions that prevent it from running on my P90.
    git-svn-id: https://svn.code.sf.net/p/fmslogo/source@3279 9d0050d7-00b0-40f9-871c-8e02cbd5794f

--- trunk/logo/wxwidgets/configure-wxwidgets.sh
+++ trunk/logo/wxwidgets/configure-wxwidgets.sh
@@ -1,30 +1,41 @@
 #! bash
-
 ###########################################################################
 # This file is a reminder of how I built wxwidgets on Windows.
 #
 # Use the script like:
 #   bash
-#   rm configarg.cache
 #   cd <wxwidgets_top_dir>
 #   <path_to_this_script>/configure-wxwidgets.sh
 #   gmake clean all
 #   gmake -C contrib/src/stc clean all
 #
-#
-# Note that these configuration options will not build an executable that is compatible
-# with Windows 95.  More specifically, the i686 MinGW runtime uses a CMOVNBE instruction
-# in a static initializer.  CMOVcc is only supported on Pentium Pro CPUs and higher.
-# Since Windows 95 is supported on lower CPUs, this can potentially cause FMSLogo to
-# execute an illegal instruction.  For reference, the opcode is
-#
-#   0x0F 0x48
-#
-# To address this, the code must be compiled with a --host that uses a GCC from an older
-# version of MinGW (3.4.2 seems to work).
+###########################################################################
 
 export PATH=/usr/bin:$PATH
 export MAKE=gmake
+
+#
+# The minimum system requirements for the OWL FMSLogo are Windows 95, running
+# on a Pentium.  To prevent the compiler from generating instructions that
+# are not supported on a Pentium, we specify -march=i586.
+#
+# Without this, the i686 MinGW compiler emits a CMOVNBE instruction in a
+# static initializers. Since CMOVcc is only supported on Pentium Pro CPUs
+# and higher, the result is that FMSLogo raises an unknown instruction
+# exception on startup when run on a Pentium.
+#
+# For reference, the opcode of a CMOVcc is
+#
+#   0x0F 0x48 ..
+#
+export CFLAGS=-march=i586
+export CXXFLAGS=-march=i586
+
+if [ -f configarg.cache ];
+then
+  rm configarg.cache
+fi
+
 
 if [ "$DEBUG" == "1" ];
 then
@@ -33,27 +44,36 @@ else
   export ADDITIONAL_OPTIONS="--disable-debug --enable-optimise"
 fi
 
-./configure                \
-    $ADDITIONAL_OPTIONS    \
-    --with-msw             \
-    --with-expat=builtin   \
-    --enable-monolithic    \
-    --disable-shared       \
-    --disable-sockets      \
-    --disable-protocols    \
-    --disable-ftp          \
-    --disable-http         \
-    --disable-tarstream    \
-    --disable-mdi          \
-    --disable-tipdlg       \
-    --disable-largefile    \
-    --disable-tga          \
-    --disable-pcx          \
-    --disable-iff          \
-    --disable-pnm          \
-    --disable-wizarddlg    \
-    --with-libstc          \
-    --with-libstl          \
-    --with-libjpeg=no      \
-    --enable-no_deps       \
-    --host=i686-pc-mingw32 \
+./configure                       \
+    $ADDITIONAL_OPTIONS           \
+    --with-msw                    \
+    --with-expat=no               \
+    --with-regex=no               \
+    --with-libpng=no              \
+    --with-zlib=no                \
+    --with-libjpeg=no             \
+    --with-libtiff=no             \
+    --enable-monolithic           \
+    --disable-shared              \
+    --disable-sockets             \
+    --disable-protocols           \
+    --disable-protocol-http       \
+    --disable-ftp                 \
+    --disable-http                \
+    --disable-fileproto           \
+    --disable-url                 \
+    --disable-tarstream           \
+    --disable-mdi                 \
+    --disable-tipdlg              \
+    --disable-largefile           \
+    --disable-tga                 \
+    --disable-pcx                 \
+    --disable-iff                 \
+    --disable-pnm                 \
+    --disable-png                 \
+    --disable-zipstream           \
+    --disable-wizarddlg           \
+    --disable-threads             \
+    --enable-no_deps              \
+    --disable-dependency-tracking \
+    --host=i686-pc-mingw32        \
