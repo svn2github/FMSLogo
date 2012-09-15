77605e5013b448a175dea6e5d755af865569cce6
david_costanzo <david_costanzo@9d0050d7-00b0-40f9-871c-8e02cbd5794f>
2012-09-15 20:16:23 +0000

    Add "UNSTABLE" to the version in the wxWidgets about box so that there's an easy way for users to distinguish the wxWidgets version from the OWL version.  The wxWidgets version has not undergone enough beta testing to be the official  version.
    git-svn-id: https://svn.code.sf.net/p/fmslogo/source@3338 9d0050d7-00b0-40f9-871c-8e02cbd5794f

--- trunk/logo/wxwidgets/aboutfmslogo.cpp
+++ trunk/logo/wxwidgets/aboutfmslogo.cpp
@@ -11,22 +11,29 @@
 // CAboutFmsLogo
 // ----------------------------------------------------------------------------
 
+#define LOCALIZED_ABOUTFMS_UNSTABLE_VERSION        \
+    LOCALIZED_GENERAL_PRODUCTNAME                  \
+    "Version "                                     \
+    FMSLOGO_VERSION " - UNSTABLE\n"                \
+    "http:\x2F/sourceforge.net/projects/fmslogo"   \
+
 CAboutFmsLogo::CAboutFmsLogo(wxWindow *parent)
     : wxDialog(parent, wxID_ANY, wxString(LOCALIZED_ABOUTFMS))
 {
     wxBoxSizer *sizerTop = new wxBoxSizer(wxVERTICAL);
 
+    
     // The text in all of the static controls
     static const char * staticText[] = {
-        "  " LOCALIZED_ABOUTFMS_VERSION         "  ",
-        "  " LOCALIZED_ABOUTFMS_GUI             "  ",
-        "  " LOCALIZED_ABOUTFMS_CORE            "  ",
-        "  " LOCALIZED_ABOUTFMS_INSTALLER       "  ",
-        "  " LOCALIZED_ABOUTFMS_ADVENTURE       "  ",
-        "  " LOCALIZED_ABOUTFMS_SPECIALTHANKS   "  ",
-        "  " LOCALIZED_ABOUTFMS_GPL             "  ",
-        "  " LOCALIZED_ABOUTFMS_NEWSGROUP       "  ",
-        "  " LOCALIZED_ABOUTFMS_MULTIMEDIALOGIC "  ",
+        "  " LOCALIZED_ABOUTFMS_UNSTABLE_VERSION "  ",
+        "  " LOCALIZED_ABOUTFMS_GUI              "  ",
+        "  " LOCALIZED_ABOUTFMS_CORE             "  ",
+        "  " LOCALIZED_ABOUTFMS_INSTALLER        "  ",
+        "  " LOCALIZED_ABOUTFMS_ADVENTURE        "  ",
+        "  " LOCALIZED_ABOUTFMS_SPECIALTHANKS    "  ",
+        "  " LOCALIZED_ABOUTFMS_GPL              "  ",
+        "  " LOCALIZED_ABOUTFMS_NEWSGROUP        "  ",
+        "  " LOCALIZED_ABOUTFMS_MULTIMEDIALOGIC  "  ",
     };
 
     for (size_t i = 0; i < ARRAYSIZE(staticText); i++)
