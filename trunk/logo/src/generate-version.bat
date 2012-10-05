4ba35d2b1264304d97d02e299a4e69e3286627fa
david_costanzo <david_costanzo@9d0050d7-00b0-40f9-871c-8e02cbd5794f>
2012-10-05 18:58:22 +0000

    Make it so that the wxwidgets project can build version.h, if necessary.
    git-svn-id: https://svn.code.sf.net/p/fmslogo/source@3443 9d0050d7-00b0-40f9-871c-8e02cbd5794f

--- trunk/logo/src/generate-version.bat
+++ trunk/logo/src/generate-version.bat
@@ -1,8 +1,8 @@
-if exist version.h del version.h
-echo #define FMSLOGO_MAJOR_VERSION %1 >> version.h
-echo #define FMSLOGO_MINOR_VERSION %2 >> version.h
-echo #define FMSLOGO_MICRO_VERSION %3 >> version.h
+if exist "%~dp0version.h" del "%~dp0version.h"
+echo #define FMSLOGO_MAJOR_VERSION %1 >> "%~dp0version.h"
+echo #define FMSLOGO_MINOR_VERSION %2 >> "%~dp0version.h"
+echo #define FMSLOGO_MICRO_VERSION %3 >> "%~dp0version.h"
 if NOT "%4"=="" (
-    echo #define FMSLOGO_PRIVATE_BUILD >> version.h
+    echo #define FMSLOGO_PRIVATE_BUILD >> "%~dp0version.h"
 )
-echo #define FMSLOGO_VERSION "%1.%2.%3%4" >> version.h
+echo #define FMSLOGO_VERSION "%1.%2.%3%4" >> "%~dp0version.h"
