5d70a20269fa4097586881727d88d8686b34ace0
david_costanzo <david_costanzo@9d0050d7-00b0-40f9-871c-8e02cbd5794f>
2015-06-15 00:43:31 +0000

    Change the phrasing of the FMSLogo Extras to fit the imperative style of the rest of the links.
    git-svn-id: https://svn.code.sf.net/p/fmslogo/source@3874 9d0050d7-00b0-40f9-871c-8e02cbd5794f

--- trunk/www/aside.php
+++ trunk/www/aside.php
@@ -5,9 +5,9 @@
 			<li><a href="http://fmslogo.sourceforge.net/media/demovideo.mp4">Watch the Demo</a></li>
 			<li><a href="http://sourceforge.net/p/fmslogo/">Download FMSLogo</a></li>
 			<li><a href="http://fmslogo.sourceforge.net/workshop">Play with the Turtle</a></li>
+			<li><a href="https://sourceforge.net/projects/fmslogo/files/Supplemental%20Logo%20Programs/">Get Extra Goodies</a></li>
 			<li><a href="http://sourceforge.net/p/fmslogo/bugs/">Report a Bug</a></li>
 			<li><a href="http://sourceforge.net/p/fmslogo/feature-requests/">Post a Feature Request</a></li>
-			<li><a href="https://sourceforge.net/projects/fmslogo/files/Supplemental%20Logo%20Programs/">FMSLogo Extras</a></li>
 		</ul>
 	</section>
 </aside>
