6f9ff1e1f4a21fe4b5aac3e54818f687a538b4b0
david_costanzo <david_costanzo@9d0050d7-00b0-40f9-871c-8e02cbd5794f>
2015-06-15 01:35:02 +0000

    Update the FMSLogo Extras link to point directly to the .zip file. Pointing to the folder is more general, but SourceForge doesn't make it obvious what to do next and, in fact, misdirects people to "download the latest version" (of FMSlogo, not the extras).
    git-svn-id: https://svn.code.sf.net/p/fmslogo/source@3876 9d0050d7-00b0-40f9-871c-8e02cbd5794f

--- trunk/www/aside.php
+++ trunk/www/aside.php
@@ -5,7 +5,7 @@
 			<li><a href="http://fmslogo.sourceforge.net/media/demovideo.mp4">Watch the Demo</a></li>
 			<li><a href="http://sourceforge.net/p/fmslogo/">Download FMSLogo</a></li>
 			<li><a href="http://fmslogo.sourceforge.net/workshop">Play with the Turtle</a></li>
-			<li><a href="https://sourceforge.net/projects/fmslogo/files/Supplemental%20Logo%20Programs/">Get Extra Goodies</a></li>
+			<li><a href="https://sourceforge.net/projects/fmslogo/files/Supplemental%20Logo%20Programs/FMSLogo%20Extras%201.10/fmslogo-extras-1.10.zip/download">Get Extra Goodies</a></li>
 			<li><a href="http://sourceforge.net/p/fmslogo/bugs/">Report a Bug</a></li>
 			<li><a href="http://sourceforge.net/p/fmslogo/feature-requests/">Post a Feature Request</a></li>
 		</ul>
