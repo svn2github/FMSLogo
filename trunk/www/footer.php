dde6bd80ddb830c2e5be199cddd6cf33cd717233
david_costanzo <david_costanzo@9d0050d7-00b0-40f9-871c-8e02cbd5794f>
2015-06-18 22:55:39 +0000

    Add the use of a character entity, which Firefox made red in View Source (indicating an HTML error)
    git-svn-id: https://svn.code.sf.net/p/fmslogo/source@3882 9d0050d7-00b0-40f9-871c-8e02cbd5794f

--- trunk/www/footer.php
+++ trunk/www/footer.php
@@ -1,7 +1,7 @@
 <footer>
     <a href="http://sourceforge.net/projects/fmslogo"><img
         id="sourceforge"
-        src="http://sflogo.sourceforge.net/sflogo.php?group_id=149686&type=13"
+        src="http://sflogo.sourceforge.net/sflogo.php?group_id=149686&amp;type=13"
         width="120"
         height="30"
         alt="Get FMSLogo at SourceForge.net. Fast, secure and Free Open Source software downloads" /></a>
