2136f603b8eaf4f582cff838ee2dfa1dd9610c39
david_costanzo <david_costanzo@9d0050d7-00b0-40f9-871c-8e02cbd5794f>
2015-11-01 22:03:49 +0000

    Fix a bug in the logic that ignored a shebang line (#!), where it would treat non-string words (integers and floating point numbers) as strings.  The code was incorrect but previously had no practical impact, as it had a strange check for a NULL string pointer, which happened to prevent illegal access because newnode would fill car/cdr/obj with NIL.
    However, this bug now caused a crash due to a recent optimization in newnode() where it no longer initialized the car/cdr/obj.  The fix is to check for a string node, intsead of word node.  This makes the code more correct, clearer, smaller, and faster.
    
    
    git-svn-id: https://svn.code.sf.net/p/fmslogo/source@3947 9d0050d7-00b0-40f9-871c-8e02cbd5794f

--- trunk/logo/src/parse.cpp
+++ trunk/logo/src/parse.cpp
@@ -1139,9 +1139,8 @@ NODE *runparse(NODE *ndlist)
     if (ndlist != NIL)
     {
         NODE * curnd = car(ndlist);
-        if (is_word(curnd)             && 
+        if (is_string(curnd)           && 
             getstrlen(curnd) >= 2      &&
-            getstrptr(curnd) != NULL   && 
             getstrptr(curnd)[0] == '#' && 
             getstrptr(curnd)[1] == '!')
         {
