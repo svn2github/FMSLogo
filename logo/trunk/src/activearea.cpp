6da1f273e9841d10498626950546fd2653a8cdee
david_costanzo <david_costanzo@9d0050d7-00b0-40f9-871c-8e02cbd5794f>
2016-07-20 03:10:29 +0000

    Fix bug #319; SETACTIVEAREA now throws a recoverable error when given points that define a zero or negative area rectangle.  Before, the error was not recoverable.
    Also extend the ERRACT test infrastructure to support testing giving a command bad input (before, it requires that the bad instructions output a value).  This involved rewriting some of the existing callers.
    
    git-svn-id: https://svn.code.sf.net/p/fmslogo/source@4187 9d0050d7-00b0-40f9-871c-8e02cbd5794f

--- logo/trunk/src/activearea.cpp
+++ logo/trunk/src/activearea.cpp
@@ -55,42 +55,44 @@ bool IsActiveAreaOneToOneWithScreen()
     return isOneToOne;
 }
 
-NODE *lsetactivearea(NODE *arg)
+NODE *lsetactivearea(NODE *args)
 {
-    NODE * args = int_vector_4_arg(arg);
-    if (stopping_flag == THROWING)
+    NODE * vector = int_vector_4_arg(args);
+    while (NOT_THROWING)
     {
-        return Unbound;
+        // All arguments are integers.
+        const NODE * xLowNode  = car(vector);
+        const NODE * yLowNode  = car(cdr(vector));
+        const NODE * xHighNode = car(cdr(cdr(vector)));
+        const NODE * yHighNode = car(cdr(cdr(cdr(vector))));
+
+        const int xLow  = getint(xLowNode);
+        const int yLow  = getint(yLowNode);
+        const int xHigh = getint(xHighNode); 
+        const int yHigh = getint(yHighNode); 
+
+        // Confirm that the four integers defines a positive area.
+        if (xLow < xHigh && yLow < yHigh)
+        {
+            // Now that we have validated the input, we can commit to it
+            g_PrinterAreaXLow  = xLow;
+            g_PrinterAreaYLow  = yLow;
+            g_PrinterAreaXHigh = xHigh; 
+            g_PrinterAreaYHigh = yHigh; 
+
+            SetConfigurationInt("Printer.XLow",   g_PrinterAreaXLow);
+            SetConfigurationInt("Printer.XHigh",  g_PrinterAreaXHigh);
+            SetConfigurationInt("Printer.YLow",   g_PrinterAreaYLow);
+            SetConfigurationInt("Printer.YHigh",  g_PrinterAreaYHigh);
+            break;
+        }
+
+        // This vector of four integers does not define a positive area.
+        // Throw a recoverable bad data error to give the programmer
+        // a chance to replace it with good data.
+        setcar(args, err_logo(BAD_DATA, vector));
+        vector = int_vector_4_arg(args);
     }
-
-    // All arguments are integers.
-    const NODE * xLowNode  = car(args);
-    const NODE * yLowNode  = car(cdr(args));
-    const NODE * xHighNode = car(cdr(cdr(args)));
-    const NODE * yHighNode = car(cdr(cdr(cdr(args))));
-
-    const int xLow  = getint(xLowNode);
-    const int yLow  = getint(yLowNode);
-    const int xHigh = getint(xHighNode); 
-    const int yHigh = getint(yHighNode); 
-
-    if (xHigh <= xLow || yHigh <= yLow)
-    {
-        // TODO: make this a recoverable error
-        err_logo(BAD_DATA_UNREC, args);
-        return Unbound;
-    }
-
-    // Now that we have validated the input, we can commit to it
-    g_PrinterAreaXLow  = xLow;
-    g_PrinterAreaYLow  = yLow;
-    g_PrinterAreaXHigh = xHigh; 
-    g_PrinterAreaYHigh = yHigh; 
-
-    SetConfigurationInt("Printer.XLow",   g_PrinterAreaXLow);
-    SetConfigurationInt("Printer.XHigh",  g_PrinterAreaXHigh);
-    SetConfigurationInt("Printer.YLow",   g_PrinterAreaYLow);
-    SetConfigurationInt("Printer.YHigh",  g_PrinterAreaYHigh);
 
     return Unbound;
 }
