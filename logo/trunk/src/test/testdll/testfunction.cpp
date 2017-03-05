f6b7807d491b757a2b9930b28e84a6060961f92a
david_costanzo <david_costanzo@9d0050d7-00b0-40f9-871c-8e02cbd5794f>
2017-03-05 17:10:48 +0000

    Add a test case for DLLCALL when the string returned from the native function contains spaces.  In this case, DLLCALL returns each word as a separate element in the returned list.
    git-svn-id: https://svn.code.sf.net/p/fmslogo/source@4298 9d0050d7-00b0-40f9-871c-8e02cbd5794f

--- logo/trunk/src/test/testdll/testfunction.cpp
+++ logo/trunk/src/test/testdll/testfunction.cpp
@@ -52,7 +52,7 @@ FormatFloat(double Number)
 //
 extern "C"
 __declspec(dllexport)
-char *
+PSTR
 WINAPI
 FormatWord(WORD Number)
 {
@@ -65,4 +65,49 @@ FormatWord(WORD Number)
 
     // Return the string
     return formattedWord;
+}
+
+
+// A simple function that applies the "rot13" algorithm to a string.
+//
+// It can be called like
+//   DLLCALL [S UpperCase@4 S MyString ]
+//
+extern "C"
+__declspec(dllexport)
+PSTR
+WINAPI
+Rot13(PCSTR String)
+{
+    // Allocate a buffer to hold the return string
+    size_t stringLength = strlen(String);
+    char * rot13String  = static_cast<char*>(GlobalAlloc(GPTR, stringLength + 1));
+
+    // Copy and translate the DLL.
+    for (int i = 0; i <= stringLength; i++)
+    {
+        int ch = String[i];
+        if ('A' <= ch && ch <= 'Z')
+        {
+            ch += 13;
+            if ('Z' < ch)
+            {
+                ch -= 26;
+            }
+        }
+        else if ('a' <= ch && ch <= 'z')
+        {
+            ch += 13;
+            if ('z' < ch)
+            {
+                ch -= 26;
+            }
+        }
+
+        rot13String[i] = ch;
+    }
+    rot13String[stringLength] = '\0';
+
+    // Return the string
+    return rot13String;
 }
